92dcbdb7beed9baf7977b9d652ee788857d2bb67
nipun2403 <nipun24sharma@gmail.com>
2023-02-04 22:24:41 +0530

    Blog Website v1 Completed

--- web-projects/blog-website/app.js
+++ web-projects/blog-website/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash")
 
 // Sample Text
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,15 +17,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-// Decalring Variables
-const compose =[];
+// Decalring Variables and Functions
+var posts =[];
+
+// String Truncating Function
+function truncateString(str, num) {
+  if (str.length <= num) {
+    return str
+  }
+  return str.slice(0, num) + '...'
+}
+
 
 
 
 // Home Route
 app.get("/", function(req,res)
 {
-  res.render('home', {homeContent: homeStartingContent});
+  res.render('home', {homeContent: homeStartingContent, posts: posts});
 
 
 });
@@ -49,25 +59,48 @@ app.get("/compose", function(req,res)
   res.render('compose')
 });
 
-// New Compose Post Request
 
+// New Post Request
 app.post("/", function(req,res)
 {
 
-  let post = {
+  // defining post json
+  const post = {
     title: req.body.postTitle,
     body: req.body.postBody
-  };
+  };  
+
+  // pusing json post into global posts array
+  posts.push(post)
   
-
-
-  console.log(post);
+  // console.log(posts);
   // compose.push(composeItem);
 
+  res.redirect("/compose");
 });
 
-
-
+app.get('/posts/:id', function(req,res)
+{ 
+  // console.log(req.params.id);
+  let requestedTitle = _.lowerCase(req.params.id);
+  posts.forEach(function(post)
+  {
+    let actualTitle = _.lowerCase(post.title)
+    if (actualTitle ===requestedTitle )
+    {
+      res.render('post', {title: post.title, body: post.body  })
+      // console.log("Match Found"); 
+      
+    }
+    else
+    {
+      res.redirect("/")
+      
+    }
+  })
+  
+  // res.redirect("/")
+})
 
 
 

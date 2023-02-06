dc9824d3ebe2c4191a708afa298b9404ae4b3958
nipun2403 <nipun24sharma@gmail.com>
2023-02-06 20:29:25 +0530

    Weather project re-touch

--- web-projects/weather-project/app.js
+++ web-projects/weather-project/app.js
@@ -5,11 +5,14 @@ const https = require("https");
 const bodyParser = require("body-parser")
 
 app.use(bodyParser.urlencoded({extended : true}));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 
 app.get("/", function(req,res)
 {
 
-  res.sendFile(__dirname + "/index.html")
+  res.render('index');
 
 });
 
@@ -30,25 +33,34 @@ https.get(url, function(responce)
   {
 
     
-
     const weatherData = JSON.parse(data);
+    // console.log(weatherData);
     const temp = weatherData.main.temp;
     const feelsLike = weatherData.main.feels_like;
     const description = weatherData.weather[0].description;
     const icon = weatherData.weather[0].icon;
     const imgUrl = "http://openweathermap.org/img/wn/"+ icon+"@2x.png";
 
-    res.write("<h1>The weather is " + description + "</h1> ");
-    res.write("<p>Current Temprature is : " + temp + "</p>");
-    res.write("<p>Feels like : " + feelsLike + "</p>");
-    res.write("<img src = " + imgUrl + ">");
+    // res.write("<h1>The weather is " + description + "</h1> ");
+    // res.write("<p>Current Temprature is : " + temp + "</p>");
+    // res.write("<p>Feels like : " + feelsLike + "</p>");
+    // res.write("<img src = " + imgUrl + ">");
 
     // console.log("about : " + description );
     // console.log(icon);
     // console.log(temp);
     // console.log("Feels like : " + feelsLike);
 
-    res.send();
+    let result = {
+      cityName: query,
+      weather: description,
+      temprature: temp,
+      feels: feelsLike,
+      src:imgUrl
+    }
+
+
+    res.render('result', {result})
   })
 
 });
